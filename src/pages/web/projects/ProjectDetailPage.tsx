7285139d9b37b9bf0255f8a96f90d8982ac9c057
김수빈 <rolling0321@5finger.co.kr>
2026-01-06 15:32:37 +0900

    [feat] 프로젝트 디테일 페이지 추가

---
+++ src/pages/web/projects/ProjectDetailPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProjectDetailPage(){
+  return (
+    <div>
+      프로젝트 디테일 페이지입니다.
+    </div>
+  )
+}
+
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,43 +4,44 @@ import ErrorPage from '@/pages/ErrorPage';
 import MainPage from '@/pages/web/MainPage';
 import ProjectListPage from '@/pages/web/projects/ProjectListPage';
 import AuthLayout from '@/layout/AuthLayout';
+import ProjectDetailPage from '@/pages/web/projects/ProjectDetailPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainLayout/>,
-    errorElement: <ErrorPage/>, // 에러 발생 시 보여줄 페이지
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <MainPage/>,
+        element: <MainPage />,
       },
       {
         path: 'projects',
         children: [
-            {
-                index: true,
-                element: <ProjectListPage/>
-            },
-            // {
-            //     path: 'detail',
-            //     element: <ProjectdetilaPage/>
-            // },
+          {
+            index: true,
+            element: <ProjectListPage />
+          },
+          {
+            path: 'detail',
+            element: <ProjectDetailPage />
+          },
         ]
       },
     ],
   },
   {
     path: '/auth',
-    element: <AuthLayout/>,
-    errorElement: <ErrorPage/>,
+    element: <AuthLayout />,
+    errorElement: <ErrorPage />,
     children: [
-        {
-            path: 'login'
-        },
-        {
-            path: 'signIn'
-        }
+      {
+        path: 'login'
+      },
+      {
+        path: 'signIn'
+      }
     ]
   }
 ]);
