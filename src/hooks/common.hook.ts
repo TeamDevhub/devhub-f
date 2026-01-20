fad8dc10d7066d933d2e45f746c3984538cedad4
임정한 <wjdgks4752@gmail.com>
2026-01-20 11:27:53 +0900

    [feat] 묶음 상태 관리 hook 생성

---
+++ src/hooks/common.hook.ts
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export const useFormState = <T>(initialState: T) => {
+  const [state, setState] = useState<T>(initialState);
+
+  const handleChange = <K extends keyof T>(key: K, value: T[K]) => {
+    setState((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const createHandler = <K extends keyof T>(key: K) => {
+    return (value: T[K]) => handleChange(key, value);
+  };
+
+  return [state, handleChange, createHandler] as const;
+}
+
+// const [searchData, onChange, getHandler] = useFormState({
+//   keyword: '',
+//   codes: [] as string[],
+//   startDate: null as Dayjs | null
+// });
+
+// // 바로 사용
+// <input onChange={(e) => onChange('keyword', e.target.value)} />
+
+// // 함수를 넘겨줄 때
+// <MyMultiSelect onChange={getHandler('codes')} />
+// <DatePicker onChange={getHandler('startDate')} />
