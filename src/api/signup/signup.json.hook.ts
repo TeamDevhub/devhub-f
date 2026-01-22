2c8d6c1446c96b4ec24878184cfdcb53658d4157
Yu Sihwan <tlghks1217@naver.com>
2026-01-22 13:45:22 +0900

    [feat] verification, signup mock 함수 생성

--- src/api/signup/signup.json.hook.ts
+++ src/api/signup/signup.json.hook.ts
@@ -1,51 +1,33 @@
-import { useMutation, useSelect } from "@/hooks/api.hook";
+import { useMutation } from "@/hooks/api.hook";
 import type { ApiResponse } from "@/types/api.type";
-import type { ProjectListResponse, ProjectSearchRequest, UpdateProjectRequest } from "./projects.type";
+import type { SignupRequest, VerificationConfirmRequest, VerificationRequest } from './signup.type';
 
-import selectListResJSON from '@/assets/jsonData/projectsPage/projectListRes.json';
-import selectDetailResJSON from '@/assets/jsonData/projectsPage/projectDetailRes.json';
 import apiSuccessResJSON from '@/assets/jsonData/apiSuccessRes.json'
 
 // 개발용 JSON 호출
-
-//조회
-export const useSelectProjects = (req: ProjectSearchRequest) => {
-  const mockGetProjects = async (): Promise<ApiResponse<ProjectListResponse>> => {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    return selectListResJSON as ApiResponse<ProjectListResponse>;
-  };
-
-  return useSelect<ProjectListResponse, ProjectSearchRequest>({
-    apiFn: mockGetProjects,
-    req,
-    cacheKey: `projects-mock-${JSON.stringify(req)}`,
-  });
-};
-
-export const useSelectProjectDetail = (projectId?: string) => {
-  const mockGetProject = async (): Promise<ApiResponse<ProjectListResponse>> => {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    return selectDetailResJSON as ApiResponse<ProjectListResponse>;
-  };
-
-  return useSelect<ProjectListResponse, string>({
-    apiFn: mockGetProject,
-    req: projectId!,
-    enabled: !!projectId,
-    cacheKey: `project-mock-${projectId}`,
-  });
-}
-
-
-//생성 수정 삭제
-export const useUpdateProject = () => {
-  const mockUpdateProject = async (): Promise<ApiResponse<void>> => {
+export const usePublishVerification = () => {
+  const mockPublishVerification = async (): Promise<ApiResponse<void>> => {
     await new Promise((resolve) => setTimeout(resolve, 500));
     return apiSuccessResJSON as ApiResponse<void>;
   };
 
-  useMutation<{
-    projectId: string;
-    data: UpdateProjectRequest;
-  }, void>(mockUpdateProject);
-}
+  useMutation<VerificationRequest, void>(mockPublishVerification);
+};
+
+export const useConfirmVerification = () => {
+  const mockConfirmVerification = async (): Promise<ApiResponse<void>> => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return apiSuccessResJSON as ApiResponse<void>;
+  };
+
+  useMutation<VerificationConfirmRequest, void>(mockConfirmVerification);
+};
+
+export const useSignup = () => {
+  const mockSignup = async (): Promise<ApiResponse<void>> => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return apiSuccessResJSON as ApiResponse<void>;
+  };
+
+  useMutation<SignupRequest, void>(mockSignup);
+};
