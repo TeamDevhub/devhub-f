38b841062cfdc1b507f8216f1609c958e520f7e2
김수빈 <rolling0321@5finger.co.kr>
2026-01-22 10:28:57 +0900

    [chore] react swiper 추가

--- src/pages/web/mypage/projects/list/MyProjectApplicant.tsx
+++ src/pages/web/mypage/projects/list/MyProjectApplicant.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import MyInfoBox from '@/pages/web/mypage/home/MyInfoBox'
+import { Paper } from '@mui/material'
 
 export default function MyProjectApplicant(){
   return (
-    <div>
-      내가 등록한 프로젝트 신청자 리스트입니다.
+    <div className='main-page align-stretch' style={{ minHeight: 'calc(100vh - 7rem)' }}>
+      {/* 1. left area */}
+      <MyInfoBox selectedKey='projects' />
+      {/* 2. right area */}
+      <Paper className='mypage-box flex-col flex-grow' elevation={4}></Paper>
     </div>
   )
 }
