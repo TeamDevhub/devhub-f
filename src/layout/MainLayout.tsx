c1adbcffb8870bcd82bf6fd95ac1049ec9695744
임정한 <wjdgks4752@gmail.com>
2026-02-04 17:57:48 +0900

    [refactor] layout 편집

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,11 @@
 import commonCodeJson from "@/assets/jsonData/commonCode.json";
 import Header from '@/components/_common/layout/Header';
-import {injectLoadingHandler} from "@/utils/util.api";
 import {setCommonCodes} from '@/utils/util._common';
 import {useEffect} from 'react';
-import {Outlet, useNavigation} from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import {LoadingProvider} from "@/contexts/LoadingProvider";
 import {CommonCodeProvider} from "@/contexts/CommonCodeProvider.tsx";
-import {UseLoading} from "@/contexts/LoadingContext.ts";
+import {LoadingBridge} from "@/contexts/LoadingContext.ts";
 
 export default function MainLayout() {
 
@@ -16,8 +15,8 @@ export default function MainLayout() {
 
     return (
         <LoadingProvider>
+            <LoadingBridge/>
             <CommonCodeProvider>
-                <LoadingBridge/>
                 <div id='devHub' className='wh-100'>
                     <Header></Header>
                     <main>
@@ -28,21 +27,3 @@ export default function MainLayout() {
         </LoadingProvider>
     )
 }
-
-const LoadingBridge = () => {
-    const {show, hide} = UseLoading();
-    const navigation = useNavigation();
-    useEffect(() => {
-        if (navigation.state === 'loading') {
-            show();
-        } else {
-            hide();
-        }
-    }, [navigation.state, show, hide]);
-
-    useEffect(() => {
-        injectLoadingHandler({show, hide});
-    }, [show, hide]);
-
-    return null;
-};
