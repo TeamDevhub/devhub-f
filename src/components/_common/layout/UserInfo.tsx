528e3390de7db3399f5ea7501102847f49c8b5af
김수빈 <rolling0321@5finger.co.kr>
2026-02-19 14:06:08 +0900

    [design] UserInfo 메뉴 관련 스타일 추가

--- src/components/_common/layout/UserInfo.tsx
+++ src/components/_common/layout/UserInfo.tsx
@@ -1,7 +1,7 @@
 import CustomAvatar from "@/components/_common/customMUI/CustomAvatar.tsx";
-import {Notifications, Person} from "@mui/icons-material";
+import {ArrowForwardIos, CheckCircle, Create, Inbox, Logout, Notifications, Person} from "@mui/icons-material";
 import useMenu from "@/hooks/_common/useMenu.ts";
-import {IconButton, Menu, MenuItem} from "@mui/material";
+import {Button, IconButton, Menu, MenuItem} from "@mui/material";
 import {Link} from "react-router-dom";
 
 export default function UserInfo({ logout }:{
@@ -28,8 +28,35 @@ export default function UserInfo({ logout }:{
                 <IconButton onClick={notificationsClick}>
                     <CustomAvatar useBadge avatarIcon={<Notifications sx={{ fontSize: 35, color: 'primary.main' }}/>}  />
                 </IconButton>
-                <Menu anchorEl={notificationsEl} open={notificationsOpen} onClose={notificationsClose}>
-
+                <Menu className="notification-menu" anchorEl={notificationsEl} open={notificationsOpen} onClose={notificationsClose}>
+                    {/* 임시 데이터 */}
+                    {/* MenuItem에 info, error 클래스 붙이면 색상 변경되도록 scss 작성해둠 */}
+                    <MenuItem className="flex-col info">
+                        <strong className="main-text">
+                            모집하는 프로젝트에 새로운 지원자가 지원하였습니다.
+                        </strong>
+                        <div className="bottom w-100 align-center justify-between">
+                            <p className="alert-date">2025.01.01</p>
+                            <Button size="small" className="detail-button" endIcon={<ArrowForwardIos />}>상세보기</Button>
+                        </div>
+                    </MenuItem>
+                    <MenuItem className="flex-col">
+                        <strong className="main-text">
+                            기본 알림입니다.
+                        </strong>
+                        <div className="bottom w-100 align-center justify-between">
+                            <p className="alert-date">2025.01.01</p>
+                        </div>
+                    </MenuItem>
+                    <MenuItem className="flex-col error">
+                        <strong className="main-text">
+                            신고 알림입니다.
+                        </strong>
+                        <div className="bottom w-100 align-center justify-between">
+                            <p className="alert-date">2025.01.01</p>
+                            <Button size="small" className="detail-button" endIcon={<ArrowForwardIos />}>상세보기</Button>
+                        </div>
+                    </MenuItem>
                 </Menu>
             </div>
             <div>
@@ -37,31 +64,31 @@ export default function UserInfo({ logout }:{
                     <CustomAvatar bgColor='text.disabled' avatarIcon={<Person sx={{ fontSize: 24 }} />} />
                 </IconButton>
                 <Menu anchorEl={userEl} open={userOpen} onClose={userClose}>
-                    <MenuItem>
-                        <CustomAvatar bgColor='text.disabled' size={32} avatarIcon={<Person sx={{ fontSize: 20 }} />} />
+                    <MenuItem sx={{ padding: '1.4rem 1.6rem', gap: '1.6rem', borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}>
+                        <CustomAvatar size={40} bgColor='text.disabled' avatarIcon={<Person sx={{ fontSize: 20 }} />} />
                         <div>
                             <p>name</p>
-                            <p>email@</p>
+                            <p style={{ fontSize: 14, color: 'var(--text-secondary)' }}>email@</p>
                         </div>
                     </MenuItem>
-                    <MenuItem onClick={userClose} component={Link} to="/">
-                        <Person></Person>
+                    <MenuItem onClick={userClose} component={Link} to="/" sx={{ marginTop: '0.8rem' }}>
+                        <Person sx={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.56)' }} />
                         <p>내 정보</p>
                     </MenuItem>
                     <MenuItem onClick={userClose} component={Link} to="/">
-                        <Person></Person>
+                        <Inbox sx={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.56)' }} />
                         <p>모집 현황</p>
                     </MenuItem>
                     <MenuItem onClick={userClose} component={Link} to="/">
-                        <Person></Person>
+                        <CheckCircle sx={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.56)' }} />
                         <p>신청 결과 확인</p>
                     </MenuItem>
                     <MenuItem onClick={userClose} component={Link} to="/">
-                        <Person></Person>
+                        <Create sx={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.56)' }} />
                         <p>프로젝트 모집하기</p>
                     </MenuItem>
-                    <MenuItem onClick={logout}>
-                        <Person></Person>
+                    <MenuItem onClick={logout} sx={{ marginBottom: '0.8rem' }}>
+                        <Logout sx={{ fontSize: 24, color: 'rgba(0, 0, 0, 0.56)' }} />
                         <p>Sign out</p>
                     </MenuItem>
                 </Menu>
