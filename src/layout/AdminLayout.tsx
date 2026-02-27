f0834e78e013f73477414c4eef068df354392d40
김수빈 <rolling0321@5finger.co.kr>
2026-02-27 10:49:30 +0900

    [design] 임시 AdminLayout 스타일 수정

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -17,7 +17,7 @@ export default function AdminLayout() {
                         <div id='devHubAdmin' className='wh-100'>
                           {/* 어드민 헤더 현재 미정 -> 임시 헤더 적용 */}
                             {/* <Header></Header> */}
-                            <div className='align-center' style={{ position: 'fixed', width: '100%', height: '6.4rem', padding: '0 2.4rem', backgroundColor: 'var(--primary-main)' }}>
+                            <div className='align-center' style={{ position: 'fixed', zIndex: 100, width: '100%', height: '6.4rem', padding: '0 2.4rem', backgroundColor: 'var(--primary-main)' }}>
                                 <h1 style={{ color: '#fff' }}>DevHub</h1>
                             </div>
                             <main>
