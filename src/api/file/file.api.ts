704295ab793858b82525b7c09fc3f75e5c76c79c
심세현 <ssehn9327@gmail.com>
2026-03-11 16:32:22 +0900

    [refactor] 파일삭제 api 수정

--- src/api/file/file.api.ts
+++ src/api/file/file.api.ts
@@ -2,7 +2,7 @@ import fetcher from "@/utils/util.api";
 
 export const deleteFile = async (req: string) => 
     await fetcher<void, string>(
-        `/projects/${req}`,
+        `/files/${req}`,
         undefined,
         { method: "delete" }
 );
