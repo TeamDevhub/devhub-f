319612bd4741344d1ba2bc69cceef23e5ae6eed3
Goonerd17 <tlghks1217@naver.com>
2026-03-24 22:08:42 +0900

    [feat] 회원가입 시 약관 목록 추가

---
+++ src/types/type.terms.ts
@@ -0,0 +1,21 @@
+export interface Terms {
+  termsGuid: string;
+  title: string;
+  content: string;
+  isRequired: boolean;
+  isUsed: boolean;
+  isDeleted: boolean;
+}
+
+export interface AgreeTermsRequest {
+  termsGuid: string;
+  isAgreed: boolean;
+}
+
+export interface CreateTermsRequest {
+  title: string;
+  content: string;
+  isRequired: boolean;
+  isUsed: boolean;
+  isDeleted: boolean;
+}
