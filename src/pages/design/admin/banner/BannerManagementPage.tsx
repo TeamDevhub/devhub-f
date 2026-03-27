25c12ef699532ccefad0f57fbf4742f586979869
김수빈 <rolling0321@5finger.co.kr>
2026-03-27 10:09:47 +0900

    [design] WebPopup 컴포넌트 웹과 어드민 스타일 구분

--- src/pages/design/admin/banner/BannerManagementPage.tsx
+++ src/pages/design/admin/banner/BannerManagementPage.tsx
@@ -323,90 +323,88 @@ export default function BannerManagementPage(){
         onDelete={() => {}}
         onSubmit={() => {}}
       >
-        <div className="admin-popup">
-          <div className="description-list flex-col gap-4">
-            <dl className='align-stretch gap-4'>
-              <dt style={{ height: 'auto' }}>배너 이미지</dt>
-              <dd className='w-100 flex-col gap-4' style={{ padding: '1.3rem 0.8rem' }}>
-                <div className="align-stretch gap-8">
-                  <CustomTextfield size='small' placeholder='이미지를 업로드해 주세요.' />
-                  <Button size='medium' variant='contained' color='primary'>업로드</Button>
-                </div>
-                <div className='help-text align-center'>
-                  <span className='dot'></span>
-                  파일 정보에 관련된 헬프 텍스트를 작성하는 란입니다.
-                </div>
-              </dd>
-            </dl>
-            <dl className='align-center gap-4'>
-              <dt>설명</dt>
-              <dd className='w-100'>
-                <CustomTextfield size='small' />
-              </dd>
-            </dl>
-            <dl className='align-center gap-4'>
-              <dt>링크</dt>
-              <dd className='w-100'>
-                <CustomTextfield size='small' />
-              </dd>
-            </dl>
-            <dl className='align-center gap-4'>
-              <dt>노출 상태</dt>
-              <dd className='w-100'>
-                <FormControl>
-                  <RadioGroup row aria-labelledby='expose-status-radio-group-label' defaultValue='expose'>
-                    <FormControlLabel value='expose' control={<Radio />} label='노출' />
-                    <FormControlLabel value='non-expose' control={<Radio />} label='비노출' />
-                  </RadioGroup>
-                </FormControl>
-              </dd>
-            </dl>
-            <dl className='align-center gap-4'>
-              <dt>상시 여부</dt>
-              <dd className='w-100'>
-                <FormControl>
-                  <RadioGroup row aria-labelledby='allTime-status-radio-group-label' defaultValue='true'>
-                    <FormControlLabel value='true' control={<Radio />} label='상시' />
-                    <FormControlLabel value='false' control={<Radio />} label='기간' />
-                  </RadioGroup>
-                </FormControl>
-              </dd>
-            </dl>
-            <dl className='align-center gap-4'>
-              <dt>노출 기간</dt>
-              <dd className='w-100 align-center gap-8'>
-                <DatePicker
-                  slotProps={{
-                    textField: {
-                      size: 'small',
-                      InputLabelProps: {
-                        shrink: true,
-                      }
-                    },
-                  }}
-                  sx={{
-                    '& legend': { display: 'none' },
-                    '& fieldset': { top: 0 },
-                  }}
-                />
-                <p className='seperator'>~</p>
-                <DatePicker
-                  slotProps={{
-                    textField: {
-                      size: 'small',
-                      InputLabelProps: {
-                        shrink: true,
-                      }
-                    },
-                  }}
-                  sx={{
-                    '& legend': { display: 'none' },
-                    '& fieldset': { top: 0 },
-                  }}
-                />
-              </dd>
-            </dl>
-          </div>
+        <div className="description-list flex-col gap-4">
+          <dl className='align-stretch gap-4'>
+            <dt style={{ height: 'auto' }}>배너 이미지</dt>
+            <dd className='w-100 flex-col gap-4' style={{ padding: '1.3rem 0.8rem' }}>
+              <div className="align-stretch gap-8">
+                <CustomTextfield size='small' placeholder='이미지를 업로드해 주세요.' />
+                <Button size='medium' variant='contained' color='primary'>업로드</Button>
+              </div>
+              <div className='help-text align-center'>
+                <span className='dot'></span>
+                파일 정보에 관련된 헬프 텍스트를 작성하는 란입니다.
+              </div>
+            </dd>
+          </dl>
+          <dl className='align-center gap-4'>
+            <dt>설명</dt>
+            <dd className='w-100'>
+              <CustomTextfield size='small' />
+            </dd>
+          </dl>
+          <dl className='align-center gap-4'>
+            <dt>링크</dt>
+            <dd className='w-100'>
+              <CustomTextfield size='small' />
+            </dd>
+          </dl>
+          <dl className='align-center gap-4'>
+            <dt>노출 상태</dt>
+            <dd className='w-100'>
+              <FormControl>
+                <RadioGroup row aria-labelledby='expose-status-radio-group-label' defaultValue='expose'>
+                  <FormControlLabel value='expose' control={<Radio />} label='노출' />
+                  <FormControlLabel value='non-expose' control={<Radio />} label='비노출' />
+                </RadioGroup>
+              </FormControl>
+            </dd>
+          </dl>
+          <dl className='align-center gap-4'>
+            <dt>상시 여부</dt>
+            <dd className='w-100'>
+              <FormControl>
+                <RadioGroup row aria-labelledby='allTime-status-radio-group-label' defaultValue='true'>
+                  <FormControlLabel value='true' control={<Radio />} label='상시' />
+                  <FormControlLabel value='false' control={<Radio />} label='기간' />
+                </RadioGroup>
+              </FormControl>
+            </dd>
+          </dl>
+          <dl className='align-center gap-4'>
+            <dt>노출 기간</dt>
+            <dd className='w-100 align-center gap-8'>
+              <DatePicker
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                    InputLabelProps: {
+                      shrink: true,
+                    }
+                  },
+                }}
+                sx={{
+                  '& legend': { display: 'none' },
+                  '& fieldset': { top: 0 },
+                }}
+              />
+              <p className='seperator'>~</p>
+              <DatePicker
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                    InputLabelProps: {
+                      shrink: true,
+                    }
+                  },
+                }}
+                sx={{
+                  '& legend': { display: 'none' },
+                  '& fieldset': { top: 0 },
+                }}
+              />
+            </dd>
+          </dl>
         </div>
       </WebPopup>
     </div>
